a4620ea5c3bd093c4442f5fd3adcae1f4f6f2fc5
conne <connect.krrishtiwari@gmail.com>
2025-11-16 08:15:35 +0530

    add

---
+++ src/Q4.java
@@ -0,0 +1,9 @@
+class Q4 {
+    public static void main(String[] args) {
+        String s = "level";
+        String rev = "";
+        for(int i=s.length()-1;i>=0;i--) rev += s.charAt(i);
+        if(s.equals(rev)) System.out.println("Palindrome");
+        else System.out.println("Not");
+    }
+}
