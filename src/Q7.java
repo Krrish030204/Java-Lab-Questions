4059c84bd720683bbeef335f5bac2134a9d288ec
conne <connect.krrishtiwari@gmail.com>
2025-11-16 16:31:20 +0530

    add

--- src/Q7.java
+++ src/Q7.java
@@ -1,18 +1,17 @@
-class Even extends Thread {
-    public void run() {
-        for(int i=2;i<=100;i+=2) System.out.println(i);
-    }
-}
+import java.sql.*;
 
-class Odd extends Thread {
-    public void run() {
-        for(int i=1;i<=99;i+=2) System.out.println(i);
-    }
-}
+class Q7 {
+    public static void main(String[] args) throws Exception {
 
-class Q6 {
-    public static void main(String[] args) {
-        new Even().start();
-        new Odd().start();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/labfiledb","root","root");
+
+        Statement s = c.createStatement();
+        s.executeUpdate("INSERT INTO students VALUES(1,'Krrish')");
+        s.executeUpdate("UPDATE students SET name='Updated' WHERE id=1");
+
+        System.out.println("Done");
     }
 }
